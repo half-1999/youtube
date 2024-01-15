9146b2718329d6936ec480e7510555dda69acbf2
Aman Sharma <halfengiiineeer2022@gmail.com>
2024-01-15 16:09:57 +0530

    youtube complete

---
+++ src/utils/api.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+  params: { hl: "en", gl: "US" },
+  headers: {
+    "X-RapidAPI-Key":
+      process.env.REACT_APP_YOUTUBE_API_KEY ||
+      "824011b310msh4fbb8bfe7535577p1de825jsna859ce3655d5",
+    "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+  },
+};
+
+export const fetchDataFromApi = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  return data;
+};
